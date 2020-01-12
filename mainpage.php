108cdf8982abef017990408483d2d2b23d6d30f0
Ethan Tran <Eqtrann@gmail.com>
2020-01-11 22:48:53 -0500

    Tran status report added

--- mainpage.php
+++ mainpage.php
@@ -12,11 +12,11 @@
     <body>
         <title>Capstone Winter 2020</title>
         
-        <h1 id="page-header"> Capstone Deliverable - Ian S, Ethan T, Karissa S</h1>
+        <h1 id="page-header"> Capstone Project - Ian S., Ethan T., Karissa S.</h1>
         <hr>
 
             <div class="col-sm-4" style="width:55%;">
-        <div class="panel panel-default" style="border-color: rgba(102,0,204, 1.0);">
+            <div class="panel panel-default" style="border-color: rgba(102,0,204, 1.0);">
             <div class="panel-heading" style="background-color: rgba(102,0,204, .1);"> Status Reports </div>
                 <div class="panel-body">
             <table class="table" >
@@ -81,11 +81,13 @@
                     <div class="col-sm-4">
                     <div class="panel panel-danger" style="border-color: rgba(178, 0, 1.0); border: .1em solid rgba(178, 0, 1.0); position: relative; margin-bottom: 10px; ">
                         <!-- ADD LINK TO GITHUB PROFILE HERE -->
-                        <div class="panel-heading"  style="color:white;"> My GitHub </div>  
-                        <div class="panel-body" > <li><a href="https://github.com/Eq-Tran/SE266">Capstone Repository</a></li></div>
-                        <div class="panel-body" > <li><a href="https://github.com/Eq-Tran">Ethan's Repository</a></li></div>
-                        <div class="panel-body" > <li><a href="https://github.com/HHarukana">Ian's Repository</a></li></div>
-                        <div class="panel-body" > <li><a href="https://github.com/klsmith6">Karissa's Repository</a></li></div>
+                        <div class="panel-heading"  style="color:white;"> Team Github Repositories </div>  
+                        <div class="panel-body">
+                         <li><a href="https://github.com/Eq-Tran/AS-Capstone">Capstone Repository</a></li>
+                         <li><a href="https://github.com/Eq-Tran">Ethan's Repository</a></li>
+                         <li><a href="https://github.com/HHarukana">Ian's Repository</a></li>
+                         <li><a href="https://github.com/klsmith6">Karissa's Repository</a></li>
+                        </div>
                     </div>
                     </div>
           
@@ -110,30 +112,23 @@
                 </div>
                 
                     <div class="panel panel-warning" style="width:35%; margin-left:10%; border:1px solid rgba(244, 232, 0, 1.0); float:right; margin-right:70%;">
-                   
-                        <div class="panel-heading"style="border:1px solid rgba(244, 232, 0, .8);" >Date:</div>
-                    
+                    <div class="panel-heading" style="border:1px solid rgba(244, 232, 0, .8);" >Date:</div>
                     <div class="panel-body">
                      <?php
-
-
                             $file = "mainpage.php";
                             $mod_date=date("F d Y h:i:s A", filemtime("mainpage.php"));
 
-                            echo "Last modified " .date("F d Y h:i:s A", filemtime("mainpage.php"));
-
-
+                            echo "Last modified " .date("F / d / Y h:m:s A", filemtime("mainpage.php"));
+                           
                      ?>
                     </div>
-                    
                 </div>
-               
             </div>
 
 
 
         
-        <footer>&copy; Ethan Tran 2019</footer>
+        <footer>&copy; Ethan Tran, Ian Shippee, Karissa Smith 2020</footer>
         
    
     <script>
