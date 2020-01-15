2e4914f11fc32503267f1adf57e704ee4bf43fca
Tran <etran@labs.neit.edu>
2020-01-15 13:03:49 -0500

    Added new database credentials

---
+++ Database_Credentials.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * README
+ * 
+ * Database host: ict.neit.edu
+ * Database name: se266_ethan
+ * Database password: ethan
+ * port number: 5500
+ * Database Schema: se266_ethan
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+
+?>
