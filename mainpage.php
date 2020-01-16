f106dbaa8a0b750f801fcfb34cc649bd802b59dc
Ethan Tran <Eqtrann@gmail.com>
2020-01-16 17:04:25 -0500

    polished up our project assignments page

--- mainpage.php
+++ mainpage.php
@@ -3,7 +3,7 @@
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type ="text/css" href="mainpage.css">
+    <link rel="stylesheet" type ="text/css" href="/StyleSheetFolder/mainCSS.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
@@ -12,13 +12,17 @@
     <body>
         <title>Capstone Winter 2020</title>
         
-        <h1 id="page-header"> Capstone Project - Ian S., Ethan T., Karissa S.</h1>
+        <h1 id="page-header" style="text-align:center;"> Capstone Project - Ian S., Ethan T., Karissa S.</h1>
         <hr>
 
             <div class="col-sm-4" style="width:55%;">
             <div class="panel panel-default" style="border-color: rgba(102,0,204, 1.0);">
-            <div class="panel-heading" style="background-color: rgba(102,0,204, .1);"> Status Reports </div>
+            <div class="panel-heading" style="background-color: rgba(102,0,204, .1);"> About </div>
                 <div class="panel-body">
+                    <p>Associates Degree - Capstone Project </p>
+                    <p>Our group decided on creating a MeetUp like scheduling web application. Focusing on making it easy to schedule meetings with friends, colleagues, and family.
+                        Lorum Ipsum I ran out of words to say somebody put something smart and fancy here to take up space pls.
+                    </p>
             <table class="table" >
                 <thead>
                 <tr>
@@ -79,9 +83,9 @@
             </div>     
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
                     <div class="col-sm-4">
-                    <div class="panel panel-danger" style="border-color: rgba(178, 0, 1.0); border: .1em solid rgba(178, 0, 1.0); position: relative; margin-bottom: 10px; ">
+                    <div class="panel panel-default" style="border-color: rgba(102,0,204, 1.0); position: relative; margin-bottom:10px; ">
                         <!-- ADD LINK TO GITHUB PROFILE HERE -->
-                        <div class="panel-heading"  style="color:white;"> Team Github Repositories </div>  
+                        <div class="panel-heading"  style="background-color: rgba(102,0,204, .1);"> Team Github Repositories </div>  
                         <div class="panel-body">
                          <li><a href="https://github.com/Eq-Tran/AS-Capstone">Capstone Repository</a></li>
                          <li><a href="https://github.com/Eq-Tran">Ethan's Repository</a></li>
@@ -98,9 +102,9 @@
            
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
                 <div class="col-sm-4">
-                <div class="panel panel-primary"  >
-                    <div class="panel-heading" style="background-color:rgba(0, 121, 178, .4);"> Deliverables </div>  
-                    <div class="panel-body">
+                <div class="panel panel-default"  style="border-color: rgba(102,0,204, 1.0)">
+                    <div class="panel-heading" style="background-color:rgba(102,0,204, .1);border-color: rgba(102,0,204, 1.0)"> Deliverables </div>  
+                    <div class="panel-body" style="border-color: rgba(102,0,204, 1.0)">
                         <li><a href="">AS Capstone Proposal</a></li>
                         <li><a href="">AS Capstone Prototype</a></li>
                         <li><a href="">AS Capstone Database Design</a></li>
@@ -111,8 +115,8 @@
                     
                 </div>
                 
-                    <div class="panel panel-warning" style="width:35%; margin-left:10%; border:1px solid rgba(244, 232, 0, 1.0); float:right; margin-right:70%;">
-                    <div class="panel-heading" style="border:1px solid rgba(244, 232, 0, .8);" >Date:</div>
+                    <div class="panel panel-default" style="width:35%; margin-left:50%; margin-right:65%; border-color: rgba(102,0,204, 1.0); float:right;">
+                    <div class="panel-heading" style="background-color:rgba(102,0,204, .1);border-color: rgba(102,0,204, 1.0)" >Date:</div>
                     <div class="panel-body">
                      <?php
                             $file = "mainpage.php";
@@ -128,7 +132,7 @@
 
 
         
-        <footer>&copy; Ethan Tran, Ian Shippee, Karissa Smith 2020</footer>
+        <footer style="clear:both; text-align:center;">&copy; Ethan Tran, Ian Shippee, Karissa Smith 2020</footer>
         
    
     <script>
