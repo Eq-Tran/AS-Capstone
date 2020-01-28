e99c92bdebc1b28053fedb6747d3da14f126d8b2
Tran <etran@labs.neit.edu>
2020-01-28 14:02:10 -0500

    Tran 2:02pm 1/28

--- Users_Posts_show_data_test.php
+++ Users_Posts_show_data_test.php
@@ -2,7 +2,7 @@
     include_once (__DIR__ . '/Models/model_functions.php');
     
     $users = showUsers();
-    $posts = showPosts();
+    $posts = showAllUserPosts();
 ?>
 <!doctype html>
 <html lang="en">
@@ -22,6 +22,7 @@
                 <?php foreach($posts as $p):?>
                 <p>POST ID: <?php echo $p['postid']?></p>
                 <p>User ID: <?php echo $p['userid']?></p>
+                <p>User NAME: <?php echo $p['uname']?></p>
                 <p>POST BODY: <?php echo $p['post']?></p>
                 <?php endforeach;?>
                 
