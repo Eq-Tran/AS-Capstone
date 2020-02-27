71d13877a0a76f0f4fb07a4e0531e9c02771354b
Tran <etran@labs.neit.edu>
2020-02-27 14:54:55 -0500

    ajax fetch

--- Test_Code/Calendar/calendar.php
+++ Test_Code/Calendar/calendar.php
@@ -58,12 +58,27 @@ $week = $dt->format('W');
             
             
         </div>
+        <div class="post-box">
+            <form method="POST">
+                <input type="text" name="post" >
+                <select>
+                    <option>Mon</option>
+                    <option>Tues</option>
+                    <option>Wed</option>
+                    <option>Thur</option>
+                    <option>Fri</option>
+                    <option>Sat</option>
+                    <option>Sun</option>
+                </select>
+                
+            </form>
+        </div>
             <input type="submit" name="Add" value="Add" onclick="executeAjaxReq()">
             <input type="submit" name="Delete" value="Delete" onclick="executeAjaxReq()">
         
     </body>
     <script>
-     function executeAjaxReq(){
+     /*function executeAjaxReq(){
          
          var xhttp = new XMLHttpRequest();
          
@@ -82,8 +97,33 @@ $week = $dt->format('W');
          xhttp.open("GET", "calendarupdate.php?", true);
          xhttp.send();
          
-     }
+     }*/
     
+    // Example POST method implementation:
+    async function postData(url = "", data = {}) {
+      // Default options are marked with *
+      const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *client
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
+      return await response.json(); // parses JSON response into native JavaScript objects
+    }
+
+    postData('calendarupdate.php', { answer: 42 })
+      .then((data) => {
+        console.log(data); // JSON data parsed by `response.json()` call
+        alert("this stuff");
+        document.querySelector("td").innerHTML = JSON.stringify(data);
+      });
     
     </script>
 </html>
--- Test_Code/Calendar/calendarupdate.php
+++ Test_Code/Calendar/calendarupdate.php
@@ -5,41 +5,7 @@ include __DIR__ . '/Models/post_request_functions.php';
 
 $data = showAllUserPosts();
 
-foreach($data as $d => $user){
-    
-    echo $user['post'];    
-    
-}
+$s = json_encode($data);
+
+echo $s;
 ?>
-<!DOCTYPE HTML>
-<html lang="en">
-    <head>
-        
-    </head>
-    <body>
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <td>ID</td>
-                        <td>Uname</td>
-                        <td>PID</td>
-                        <td>Post</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach($data as $d):?>
-                    <tr>
-                        <td><?php echo $d['userid'];?></td>
-                        <td><?php echo $d['uname'];?></td>
-                        <td><?php echo $d['postid'];?></td>
-                        <td><?php echo $d['post'];?></td>
-                        
-                    </tr>
-                    <?php endforeach;?>
-                </tbody>
-            </table>
-       
-        </div>
-    </body>
-</html>
